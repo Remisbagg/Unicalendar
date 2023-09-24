771f9ed6460a93667fd3070e083e5ff6d7b273d7
Sergio <93742269+Remisbagg@users.noreply.github.com>
2023-09-24 10:02:32 -0500

    Changes

---
+++ BDscripts/Register.php
@@ -0,0 +1,16 @@
+<?php
+$con= mysqli_connect("localhost","root","","usuarios");
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$username = $_POST["username"];
+$password = $_POST["password"];
+$statement = mysqli_prepare($con, "INSERT INTO user (name, email, username, password) VALUES (?, ?, ?, ?)");
+mysqli_stmt_bind_param($statement, "siss", $name, $email, $username, $password);
+mysqli_stmt_execute($statement);
+
+$response=array();
+$response["success"]=true;
+
+echo json_encode($response);
+?>
